492dec9c433ae2ebbe5134f24b04eaabef631fd6
arcb <arcb@example.com>
2026-07-05 02:03:47 +0300

    feat(market_data): add constructor

--- app/core/market_data.py
+++ app/core/market_data.py
@@ -2,11 +2,14 @@ import random
 
 
 class MarketData:
+    def __init__(self):
+        self._exchange = None
+
     def get_price(self, symbol: str):
         base = 42000  # fake BTC base price
         fluctuation = random.uniform(-500, 500)
 
         return {
             "symbol": symbol,
-            "price": round(base + fluctuation, 2)
+            "price": round(base + fluctuation, 2),
         }
