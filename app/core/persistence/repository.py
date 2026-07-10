87a069b1d406676d4255493281a4e15a529839a7
arcb <arcb@example.com>
2026-07-10 16:46:03 +0300

    feat: add position repository

---
+++ app/core/persistence/repository.py
@@ -0,0 +1,48 @@
+from sqlalchemy.orm import Session
+
+from app.core.persistence.models import PositionEntity
+
+
+class PositionRepository:
+    def __init__(
+        self,
+        session: Session,
+    ) -> None:
+        self._session = session
+
+    def save(
+        self,
+        position: PositionEntity,
+    ) -> None:
+        self._session.merge(position)
+        self._session.commit()
+
+    def get(
+        self,
+        symbol: str,
+    ) -> PositionEntity | None:
+        return self._session.get(
+            PositionEntity,
+            symbol,
+        )
+
+    def delete(
+        self,
+        symbol: str,
+    ) -> bool:
+        position = self.get(symbol)
+
+        if position is None:
+            return False
+
+        self._session.delete(position)
+        self._session.commit()
+        return True
+
+    def list(
+        self,
+    ) -> list[PositionEntity]:
+        return (
+            self._session.query(PositionEntity)
+            .all()
+        )
